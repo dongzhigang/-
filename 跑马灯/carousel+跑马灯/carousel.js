1a2647ab16332f0e43cab0f6471e700493c217d0
dongzhigang <13632482567@163.com>
2019-08-16 15:22:23 +0800

    修改注释

--- 跑马灯/carousel+跑马灯/carousel.js
+++ 跑马灯/carousel+跑马灯/carousel.js
@@ -17,7 +17,7 @@ Carousel.prototype._init = function(argument){	//对象初始化
 	this.top = 0;											//移动距离
 	this.isOverflowing = false;								//判断是否溢出
 
-	//添加内容父节点后
+	
 	this._element()
 	//启动定时器
 	this._setInterval();
@@ -30,22 +30,24 @@ Carousel.prototype._parentElement = function(){
 		div.innerHTML = this.first
 	return div;
 }
-//添加内容父节点后
 Carousel.prototype._element = function(){
-	//添加内容父节点后重新初始化
+	//添加内容父节点前重新初始化
 	this.el.innerHTML = null;								//当前清空内容
 	this.el.appendChild(this._parentElement())				//重新添加内容
 	this.first = this.el.firstChild;						//重新获取滚动内容
-	if (this.default.direction === "verticality") {	//水平方向
+	if (this.default.direction === "verticality") {	//垂直方向
 		this.firstWH = this.first.offsetHeight			//内容高度，用于判断循环滚动
 		this.isOverflowing = this.el.parentNode.clientHeight < this.el.parentNode.scrollHeight
-	}else{
+	}else{		//水平方向
+		//如果是垂直方向要给内容父节点一定的宽度，防止换行
 		this.el.firstChild.style.width = this.el.firstChild.children.length * this.el.firstChild.children[0].clientWidth + "px";
 		this.firstWH = this.first.offsetWidth			//内容高度，用于判断循环滚动
-		if(this.el.parentNode.clientWidth < this.el.firstChild.children.length * this.el.firstChild.children[0].clientWidth){
+		//判断是否溢出
+		if(this.el.parentNode.clientWidth < this.el.parentNode.scrollWidth){
+			//给当前元素一定宽度
 			this.el.style.width = this.el.firstChild.children.length * this.el.firstChild.children[0].clientWidth*2 + "px";
-		}		
-		this.isOverflowing = this.el.parentNode.clientWidth < this.el.parentNode.scrollWidth;  
+			this.isOverflowing = true;
+		}
 	}
 	this.html = this.first.cloneNode(true);  //复制多一个  
 }
@@ -55,9 +57,10 @@ Carousel.prototype._setInterval = function(){
         this.el.appendChild(this.html);
         this.Interval = setInterval(function(){
             this.top --;     
-            if(Math.abs(this.top) >= this.firstWH){    
+            if(Math.abs(this.top) >= this.firstWH){   //当移动距离>=内容高度，就重新计算 
                 this.top = 0;
             }
+            //给当前元素添加样式
             if(this.default.direction === "verticality"){
                 this.el.style.transform = "translate3d(0,"+this.top+"px,0)";
             }else{
